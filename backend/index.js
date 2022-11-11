d8df5f935f17b0da0fd4a5fa09d22b027b3d9635
Siar Wahidi <siar.wahidi@gmail.com>
2022-11-11 13:31:21 -0500

    Added people route and made axios call to get data from API

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const peopleRouter = require("./routes/people");
+const peopleRouter = require("./route/people");
 
 app.use("/api/v1/people", peopleRouter);
+
+// middleware
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
